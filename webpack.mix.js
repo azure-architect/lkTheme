e5ad04cb8c453018ccf3bc0db625acb16abc5d15
azure-architect <vscoder.brad@gmail.com>
2024-02-09 10:29:30 +0800

    added browsersync

---
+++ webpack.mix.js
@@ -0,0 +1,35 @@
+const mix = require('laravel-mix');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('src/js/app.js', 'dist/js')
+   .sass('src/scss/app.scss', 'dist/css')
+   .setPublicPath('dist');
+
+// Configure BrowserSync
+mix.webpackConfig({
+    plugins: [
+        new BrowserSyncPlugin({
+            // BrowserSync options
+            host: 'localhost',
+            port: 3000,
+            proxy: 'your-local-development-url.test', // Update this to your local development URL
+            files: [
+                'dist/js/**/*.js', // Path to where webpack outputs the JS files
+                'dist/css/**/*.css', // Path to where webpack outputs the CSS files
+                './**/*.php', // Watch PHP files for changes (useful if integrating with a PHP project like WordPress)
+                // Add any other file paths you want to watch for changes
+            ]
+        })
+    ]
+});
